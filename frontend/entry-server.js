22fd638a2384c48c92cdcf9f9285604546bc6f1d
zypeh <zypeh.geek@gmail.com>
2017-06-14 21:49:49 +0800

    Removed unused part; update package.json

--- frontend/entry-server.js
+++
@@ -1,7 +0,0 @@
-import { app, router, store } from './app'
-
-const isDev = process.env.NODE_ENV !== 'production'
-
-export default context => {
-    /* TODO */    
-}
--- frontend/store/create-api-client.js
+++
@@ -1,8 +0,0 @@
-import socketio from 'socketio'
-
-const config = {
-    site: 'https://', // our API site
-    version: '/v1'
-}
-
-export default api
